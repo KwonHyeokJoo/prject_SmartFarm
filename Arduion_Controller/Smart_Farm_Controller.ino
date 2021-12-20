4b4d84e37be9271237048f33a2b66bd8b945b69a
KwonHyeokJoo <hyeokjoo1253@gmail.com>
2021-12-20 20:29:37 +0900

    revision_1.6

--- Arduion_Controller/Smart_Farm_Controller.ino
+++ Arduion_Controller/Smart_Farm_Controller.ino
@@ -19,6 +19,10 @@
 #define WINDOW_ANGLE_MAX 30         // 개폐기 최대 각도
 #define WINDOW_ANGLE_MIN 10         // 개폐기 최소 각도
 
+#define FAN_SPEED_MAX 255
+#define FAN_SPEED_MID 128
+#define FAN_SPEED_MIN 0
+
 Servo leftWindow;
 Servo rightWindow;
 
@@ -34,24 +38,32 @@ const String port = "80";
 // 토양수분, 온도1, 온도2, 습도1, 습도2, 조도,
 // 수동자동, 팬상태, 팬스피드, 왼쪽개폐기, 오른쪽개폐기, 열풍기
 // Receive ALL Data
-const uint8_t sensingDataCount = 6;
-const uint8_t controlDataCount = 6;
+const uint8_t sensingDataCount = 7; //getTargetTmp 추가
+const uint8_t controlDataCount = 6; 
 const uint8_t allDataCount = sensingDataCount + controlDataCount;
-String receiveType[allDataCount] = {"grd","tmp1","tmp2","hum1","hum2","lux",
+String receiveType[allDataCount] = {"grd","tmp1","tmp2","hum1","hum2","lux","getTargetTmp",
                                     "getManualControl","getFanState","getFanSpeed",
                                     "getLeftWindow","getRightWindow","getHeaterState"};
-int16_t receiveValue[allDataCount] = {0,0,0,0,0,0,1,0,0,0,0,0};
+int16_t receiveValue[allDataCount] = {0,0,0,0,0,0,0,1,0,0,0,0,0};
+
+//fan 시작 시간 측정
+uint64_t startFanSpeedControl;
+uint64_t endFanSpeedControl;
+const uint16_t waitFanSpeedControl = 3000;
+
 // 개폐기 초기 열림 각도 : 최대값으로 설정
 int8_t leftWindowAngle = WINDOW_ANGLE_MAX;
 int8_t rightWindowAngle = WINDOW_ANGLE_MAX;
 
-const uint64_t waitTime = 5000;
+//목표 온도 //토양습도
+uint8_t targetTemp; 
+uint8_t targetMoist = 40;
 
+//개폐기 시간 측정
 uint64_t leftStartWindowTime = millis();
 uint64_t leftEndWindowTime = millis();
 uint64_t rightStartWindowTime = millis();
 uint64_t rightEndWindowTime = millis();
-
 
 //wifi connection
 void connectWifi() {
@@ -283,8 +295,16 @@ void setup() {
 
 void loop() {
   uint8_t manualControl;
+  uint8_t oldFanSpeed = 0;
+  uint8_t fanState,fanSpeed,LeftControl,RightControl,heater;
+  //자동제어시 펌프상태 저장변수
+  uint8_t pumpState;
+  // 토양습도, 온도1, 온도2, 습도1, 습도2 값 저장받을 변수
+  uint8_t grdData,tmp1Data,tmp2Data,hum1Data,hum2Data,luxData;
   int i,j;
   String url;
+  
+  
 
   //센싱 값 입력 받기
   ///////////////////////////////////////////////////////////////////////////////
@@ -294,49 +314,102 @@ void loop() {
   }
   ///////////////////////////////////////////////////////////////////////////////
 
-  //현재 작동 상태 파악, Manual Mode인지, Auto Mode인지
+  // 전달 받은 센싱값 및 제어값 가져오기
   for(i=0; i<allDataCount; i++){
-    if(receiveType[i] == receiveType[sensingDataCount]){
+    if(receiveType[i] == "grd"){
+      grdData = receiveValue[i];
+    }
+    else if(receiveType[i] == "tmp1"){
+      tmp1Data = receiveValue[i];
+    }
+    else if(receiveType[i] == "tmp2"){
+      tmp2Data = receiveValue[i];
+    }
+    else if(receiveType[i] == "hum1"){
+      hum1Data = receiveValue[i];
+    }
+    else if(receiveType[i] == "hum2"){
+      hum2Data = receiveValue[i];
+    }
+    else if(receiveType[i] == "lux"){
+      luxData = receiveValue[i];
+    }
+    else if(receiveType[i] == "getTargetTmp"){
+      targetTemp = receiveValue[i];
+    }
+    else if(receiveType[i] == "getManualControl"){
       manualControl = receiveValue[i];
+    }
+    else if(receiveType[i] == "getFanState"){
+      fanState = receiveValue[i];
+    }
+    else if(receiveType[i] == "getFanSpeed"){
+      fanSpeed = receiveValue[i];
+    }
+    else if(receiveType[i] == "getLeftWindow"){
+      LeftControl = receiveValue[i];
+    }
+    else if(receiveType[i] == "getRightWindow"){
+      RightControl = receiveValue[i];
+    }
+    else if(receiveType[i] == "getHeaterState"){
+      heater = receiveValue[i];
+    }
+    else {
+      Serial.print(F("\r\n\r\n === err === \r\n\r\n"));
     }
   }
 
   //수동 제어
   if(manualControl == 0){
-    uint8_t fanState;
-    uint8_t fanSpeed;
-    uint8_t LeftControl;
-    uint8_t RightControl;
-    uint8_t heater;
-
-    // 제어할 data 및 종료 가져오기
-    for(i=sensingDataCount; i<allDataCount; i++){
-      if(receiveType[i] == receiveType[sensingDataCount+1]){
-        fanState = receiveValue[i];
-      }
-      else if(receiveType[i] == receiveType[sensingDataCount+2]){
-        fanSpeed = receiveValue[i];
-      }
-      else if(receiveType[i] == receiveType[sensingDataCount+3]){
-        LeftControl = receiveValue[i];
-      }
-      else if(receiveType[i] == receiveType[sensingDataCount+4]){
-        RightControl = receiveValue[i];
-      }
-      else if(receiveType[i] == receiveType[sensingDataCount+5]){
-        heater = receiveValue[i];
-      }
-    }
-
     //Fan Control
-    // 현재 fan이 가동중인데, 앱에서 정지(0)하라는 명령이 들어오면 정지
-    if(digitalRead(FANCONTROL_IN_1) == HIGH && fanState == 0){
-      moterControl(fanState, FANCONTROL_IN_1, FANCONTROL_IN_2);
+    if(fanState == 0){
+      //fan 정지
+      if(digitalRead(FANCONTROL_IN_1) == LOW){
+        //현재 fan 정지 상태
+        Serial.print(F("\r\n\r\n === 이미 정지 === \r\n\r\n"));
+      }
+      else {
+        //가동중이면 정지
+        moterControl(fanState, FANCONTROL_IN_1, FANCONTROL_IN_2);
+      }
     }
-    // 현재 fan이 정지이고, 앱에서 가동(1)하라는 명령이 들어오면 가동
-    else if(digitalRead(FANCONTROL_IN_1) == LOW && fanState == 1){
-      moterControl(fanState, FANCONTROL_IN_1, FANCONTROL_IN_2);
-      digitalWrite(FANCONTROL_SPEED, fanSpeed);   //fanSpeed 설정
+    else if(fanState == 1){
+      //fan 가동
+      if(digitalRead(FANCONTROL_IN_1) == HIGH){
+        //가동중 속도 조절
+        if(oldFanSpeed != fanSpeed){
+          if((endFanSpeedControl - startFanSpeedControl) > waitFanSpeedControl){
+            if(fanSpeed < FAN_SPEED_MIN){
+              //Fan Speed 최소값 설정(80)
+              fanSpeed = FAN_SPEED_MIN;
+              Serial.print(F("\r\n\r\n === Speed MIN === \r\n\r\n"));
+            }
+            else if (fanSpeed > FAN_SPEED_MAX){
+              fanSpeed = FAN_SPEED_MAX;
+              Serial.print(F("\r\n\r\n === Speed MAX === \r\n\r\n"));
+            }
+            else {
+              Serial.print(F("\r\n\r\n === Speed START === \r\n\r\n"));
+            }
+            analogWrite(FANCONTROL_SPEED, fanSpeed);
+          }
+          else {
+            endFanSpeedControl = millis();
+          }
+        }
+        else {
+          Serial.print(F("\r\n\r\n === 이미 가동 === \r\n\r\n"));
+        }
+      }
+      else {
+        //정지 -> 가동
+        moterControl(fanState, FANCONTROL_IN_1, FANCONTROL_IN_2);
+        analogWrite(FANCONTROL_SPEED, FAN_SPEED_MAX);
+        oldFanSpeed = FAN_SPEED_MAX;
+        startFanSpeedControl = millis();
+        endFanSpeedControl = millis();
+      }
     }
 
     // Left Window 개폐
@@ -349,9 +422,11 @@ void loop() {
         //안되면 while 사용해야됨 
         if((leftEndWindowTime - leftStartWindowTime) > (WINDOW_ANGLE_MAX *10)){
           leftStartWindowTime = millis();
-          leftWindow.write(WINDOW_ANGLE_MIN);
+          leftWindowAngle = WINDOW_ANGLE_MIN;
+          leftWindow.write(leftWindowAngle);
         }
         else {
+          Serial.print(F("\r\n\r\n === 작동 대기중 === \r\n\r\n"));
           leftEndWindowTime = millis();
         }
       }
@@ -364,9 +439,11 @@ void loop() {
       else {
         if((leftEndWindowTime - leftStartWindowTime) > (WINDOW_ANGLE_MAX *10)){
           leftStartWindowTime = millis();
-          leftWindow.write(WINDOW_ANGLE_MAX);
+          leftWindowAngle = WINDOW_ANGLE_MAX;
+          leftWindow.write(leftWindowAngle);
         }
         else {
+          Serial.print(F("\r\n\r\n === 작동 대기중 === \r\n\r\n"));
           leftEndWindowTime = millis();
         }
       }
@@ -385,9 +462,11 @@ void loop() {
         //안되면 while 사용해야됨 
         if((rightEndWindowTime - rightStartWindowTime) > (WINDOW_ANGLE_MAX *10)){
           rightStartWindowTime = millis();
-          rightWindow.write(WINDOW_ANGLE_MIN);
+          rightWindowAngle = WINDOW_ANGLE_MIN;
+          rightWindow.write(rightWindowAngle);
         }
         else {
+          Serial.print(F("\r\n\r\n === 작동 대기중 === \r\n\r\n"));
           rightEndWindowTime = millis();
         }
       }
@@ -400,9 +479,11 @@ void loop() {
       else {
         if((rightEndWindowTime - rightStartWindowTime) > (WINDOW_ANGLE_MAX *10)){
           rightStartWindowTime = millis();
-          rightWindow.write(WINDOW_ANGLE_MAX);
+          rightWindowAngle = WINDOW_ANGLE_MAX;
+          rightWindow.write(rightWindowAngle);
         }
         else {
+          Serial.print(F("\r\n\r\n === 작동 대기중 === \r\n\r\n"));
           rightEndWindowTime = millis();
         }
       }
@@ -420,7 +501,58 @@ void loop() {
     }
   }
   //자동 제어
-  else if(manualControl == 1){
+  else if(manualControl == 1){ 
+    uint8_t tempAvg;
+    tempAvg = (tmp1Data + tmp2Data)/2;
     
+    //Fan Control(Auto)
+    //온도 앞 뒤 차이가 5도 이상이면 팬을 가동하여 공기 순환
+    if(abs(tmp1Data - tmp2Data) >= 5) {
+      fanState = 1;
+      analogWrite(FANCONTROL_SPEED,FAN_SPEED_MAX);
+      moterControl(fanState, FANCONTROL_IN_1, FANCONTROL_IN_2);
+    } 
+    else if (abs(tmp1Data - tmp2Data) >= 3) {
+      fanState = 1;
+      analogWrite(FANCONTROL_SPEED,FAN_SPEED_MID);
+      moterControl(fanState, FANCONTROL_IN_1, FANCONTROL_IN_2);  
+    }
+    else {
+      fanState = 0;
+      moterControl(fanState, FANCONTROL_IN_1, FANCONTROL_IN_2);
+    }
+
+    //히터(Auto)
+    //목표온도가 현재온도와 3도이상 차이날 경우 히터 On 
+    if(targetTemp - tempAvg >= 3) {
+      heater = 1;
+      relayControl(HEATER_PIN, heater);
+    } 
+    else {
+      heater = 0;
+      relayControl(HEATER_PIN, heater);
+    }
+    
+    //개폐기(Auto)
+    //목표온도가 현재온도와 3도이상 차이날 경우 개폐기 On 
+    if(tempAvg - targetTemp >= 3) {
+      leftWindowAngle = WINDOW_ANGLE_MAX;
+      rightWindowAngle = WINDOW_ANGLE_MAX;
+    } 
+    else {
+      leftWindowAngle = WINDOW_ANGLE_MIN;
+      rightWindowAngle = WINDOW_ANGLE_MIN;
+    }
   }
+  
+  //토양습도센서(Auto)
+  //목표습도보다 낮은 경우 워터펌프 가동
+  if(grdData <= targetMoist) {
+    pumpState = 1;
+    relayControl(PUMP_PIN, pumpState); 
+  } 
+  else {
+    pumpState = 0;
+    relayControl(PUMP_PIN, pumpState); 
+  }  
 }
